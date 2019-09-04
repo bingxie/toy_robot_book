2a06ad5c10fe0ec932b155dca42f3e8de99a9ed9
Bing Xie <bing708@gmail.com>
2019-09-04 16:15:21 +1000

    Add load_commands to CLI

--- lib/toy_robot_book.rb
+++ lib/toy_robot_book.rb
@@ -5,6 +5,7 @@ require 'toy_robot/robot'
 require 'toy_robot/table'
 require 'toy_robot/simulator'
 require 'toy_robot/command'
+require 'toy_robot/cli'
 
 module ToyRobotBook
   class Error < StandardError; end
