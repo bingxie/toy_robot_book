a2978f1f13897a5069707fc30b589dc3d6b0ff31
Bing Xie <bing708@gmail.com>
2019-09-06 10:26:06 +1000

    Add move, turn_left, turn_right, report to Simulator

--- lib/toy_robot/cli.rb
+++ lib/toy_robot/cli.rb
@@ -1,8 +1,16 @@
 module ToyRobot
   class CLI
+    attr_reader :simulator
+
     def load_commands(commands_file)
       File.readlines(commands_file).map do |command|
         ToyRobot::Command.process(command)
+      end
+    end
+
+    def run(commands)
+      commands.each do |command, *args|  # package
+        simulator.send(command, *args) # unpackage
       end
     end
   end
--- lib/toy_robot/simulator.rb
+++ lib/toy_robot/simulator.rb
@@ -7,9 +7,23 @@ module ToyRobot
     end
 
     def place(x, y, direction)
-      if @table.valid_location?(x, y)
-        @robot = Robot.new(x, y, direction)
-      end
+      @robot = Robot.new(x, y, direction) if @table.valid_location?(x, y)
+    end
+
+    def move
+      robot.move
+    end
+
+    def turn_left
+      robot.turn_left
+    end
+
+    def turn_right
+      robot.turn_right
+    end
+
+    def report
+      robot.report
     end
   end
 end
